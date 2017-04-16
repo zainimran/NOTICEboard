b8681e9dd9d1b19ef25b223c09da61ce84c6dee7
Zain <zainimran@outlook.com>
2017-04-16 22:50:28 +0500

    Added landing page

---
+++ js/signupform.js
@@ -0,0 +1,68 @@
+var app = angular
+.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache']);
+app.controller('DemoCtrl', function($scope) {
+    $scope.user = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      city: '',
+      state: '',
+      postalCode: ''
+    };
+
+    $scope.roles = ('Student Faculty Alumni'         ).split(' ').map(function(state) {
+        return {abbrev: state};
+      });
+  })
+  .config(function($mdThemingProvider) {
+
+    // Configure a dark theme with primary foreground yellow
+
+    $mdThemingProvider.theme('docs-dark', 'default')
+      .primaryPalette('yellow')
+      .dark();
+
+  });
+
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirmPassword");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+/*app.directive('validPasswordC', function() {
+  return {
+    require: 'ngModel',
+    scope: {
+
+      reference: '=validPasswordC'
+
+    },
+    link: function(scope, elm, attrs, ctrl) {
+      ctrl.$parsers.unshift(function(viewValue, $scope) {
+
+        var noMatch = viewValue != scope.reference
+        ctrl.$setValidity('noMatch', !noMatch);
+        return (noMatch)?noMatch:!noMatch;
+      });
+
+      scope.$watch("reference", function(value) {;
+        ctrl.$setValidity('noMatch', value === ctrl.$viewValue);
+
+      });
+    }
+  }
+});
+ */
+/**
+Copyright 2016 Google Inc. All Rights Reserved. 
+Use of this source code is governed by an MIT-style license that can be foundin the LICENSE file at http://material.angularjs.org/HEAD/license.
+**/
